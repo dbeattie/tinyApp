785e6644ff9c314773b18ee94b5acf9bbe7ae602
dbeattie <darren@fettle.ca>
2019-12-09 20:16:38 +0000

    created a random string generator function and a post request route to add new short urls and their corresponding long urls to the urlDatabase

--- express_server.js
+++ express_server.js
@@ -2,7 +2,16 @@ const express = require("express");
 const app = express();
 const PORT = 8080; 
 
+//BODY PARSER MIDDLEWARE
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({extended: true}));
+
 app.set("view engine", "ejs");
+
+//PASS THE NUMBER OF CHARACTERS YOU'D LIKE TO PRODUCE
+function generateRandomString(length) {
+  return Math.round((Math.pow(36, length + 1) - Math.random() * Math.pow(36, length))).toString(36).slice(1);
+}
 
 //DATABASE
 const urlDatabase = {
@@ -15,16 +24,21 @@ app.get("/", (req, res) => {
   res.send("Hello!");
 });
 
-
 //URLS GET REQUEST
 app.get("/urls", (req, res) => {
   let templateVars = { urls: urlDatabase };
   res.render("urls_index", templateVars);
 });
 
-//NEW URLS GET REQUEST
+//NEW URLS GET REQUEST TO RENDER PAGE
 app.get("/urls/new", (req, res) => {
   res.render("urls_new");
+});
+
+app.post("/urls", (req, res) => {
+  let shortUrlKey = generateRandomString(6);
+  urlDatabase[shortUrlKey] = req.body.longURL;
+  res.redirect("/urls");    
 });
 
 //SHORT URL GET REQUEST --> NEEDS TO BE AFTER NEW URLS GET REQUEST
