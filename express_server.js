8c4bafa99ff65c72258bcbff8fd911131f7a0445
dbeattie <darren@fettle.ca>
2019-12-09 18:08:48 +0000

    added a new route for /urls to render a new ejs file urls_index with an html skeleton and some mild javascript logic

--- express_server.js
+++ express_server.js
@@ -4,32 +4,45 @@ const PORT = 8080;
 
 app.set("view engine", "ejs");
 
+//DATABASE
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
 
+//HOME PAGE
 app.get("/", (req, res) => {
   res.send("Hello!");
 });
 
+//URLS REQUEST
+app.get("/urls", (req, res) => {
+  let templateVars = { urls: urlDatabase };
+  res.render("urls_index", templateVars);
+});
+
+//LISTENING
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
 
+//GET json FILE OF URLDATABASE
 app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
 });
 
+//HELLO ROUTE
 app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
 });
 
-app.get("/set", (req, res) => {
-  const a = 1;
-  res.send(`a = ${a}`);
- });
+
+//EXAMPLE OF TWO REQUESTS THAT WON'T WORK TOGETHER DUE TO SCOPE
+// app.get("/set", (req, res) => {
+//   const a = 1;
+//   res.send(`a = ${a}`);
+//  });
  
- app.get("/fetch", (req, res) => {
-  res.send(`a = ${a}`);
- });
+//  app.get("/fetch", (req, res) => {
+//   res.send(`a = ${a}`);
+//  });
