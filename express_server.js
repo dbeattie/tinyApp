fc025539871b5528487089fd8e704560b71a1406
dbeattie <darren@fettle.ca>
2019-12-09 22:56:01 +0000

    updated index table to include a delete button
    created a POST request on the server to delete shortURL and longURL from the  database when the delete button is pressed

--- express_server.js
+++ express_server.js
@@ -55,6 +55,13 @@ app.get("/u/:shortURL", (req, res) => {
   res.redirect(longURL);
 });
 
+//DELETE BUTTON POST ON INDEX PAGE
+app.post("/urls/:shortURL/delete", (req, res) => {
+  delete urlDatabase[req.params.shortURL];
+  console.log(urlDatabase);
+  res.redirect('/urls');
+})
+
 //GET json FILE OF URLDATABASE
 app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
